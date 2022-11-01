7f51780294c07f7c232a94df34660b0aaa923902
Remix Workspace <>
2022-11-01 22:40:05 +0530

    array add

--- contracts/array.sol
+++ contracts/array.sol
@@ -25,5 +25,25 @@ contract arraysample{
 
     //Whenever we deploy a Smart Contract we will get a unique Smart Contract address.
     //for the above code/SC address is --> 0xd9145CCE52D386f254917e481eB44e9943F39138
+
+    //2. Dynamic array = Here we exactly don't know the number of students. So we use dynamic array for unlimited entries.
+    //phoneNumber[0]=9010, phoneNumber[1]=0280, phoneNumber[2]=0179.
+
+    uint[] phoneNumber;
+
+    //Write function
+    //.push() is used to make dynamic array possible. The new value will push the old number up in the stack and the index is assigned accordingly.
+    function studentPhoneNumber(uint _phoneNo) public{
+        phoneNumber.push(_phoneNo);
+    }
+
+    //Read function
+    function getPhoneNumber(uint8 _index) public view returns(uint){
+        return phoneNumber[_index];
+    }
+
+    //for the above SC address is --> 0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47
+    //Also when the info./value is starting with '0' we are unable to get 0 from the read function.
+    //In the above SC if we give index 1 the answer should be '0280' but the answer we get is '280'.
 }
 
