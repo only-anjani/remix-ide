9effddae7742eed8ad4a228e8fc0ab93190f5264
Remix Workspace <>
2022-11-01 16:53:43 +0530

    structure add

---
+++ contracts/structure.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+//Defining solidity version
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.7.0;
+
+//Defining contract/contract name
+
+contract MyContract {
+
+    
+}
