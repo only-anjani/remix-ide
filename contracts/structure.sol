09f34201a2dd22350de9797a0ed7fdd38173dbcb
Remix Workspace <>
2022-11-01 16:58:34 +0530

    structure add

--- contracts/structure.sol
+++ contracts/structure.sol
@@ -1,11 +1,11 @@
-//SPDX-License-Identifier: MIT
-//Defining solidity version
 // SPDX-License-Identifier: MIT
+//Defining solidity version
 pragma solidity >=0.6.0 <0.7.0;
 
 //Defining contract/contract name
-
 contract MyContract {
 
+    //Defining variables int (both -ve & +ve values) uint (only +ve values). public is access.
+    uint public storedData;
     
 }
