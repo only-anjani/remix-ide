0722c0a12c815c15079423b2778b40aa8b893dfa
Remix Workspace <>
2022-11-01 22:16:07 +0530

    array add

--- contracts/array.sol
+++ contracts/array.sol
@@ -8,6 +8,20 @@ contract arraysample{
     //Array always starts with the number zero, '0'.
     //Array is always indicated with '[]' , Square brackets.
     //Array --> Fixed and Dynamic
+    //1. Fixed length array = Here we already know no. of students. So, the data can only be entred for 42 students only.
+    //age[0]=26, age[1]=38, age[2]=40...Here age[0]=26 --> item[_index]=_value --> 0 is index and 26 is value.
+
+    uint8[42] age;
+
+    //Write function
+    function studentAgeData(uint8 _index, uint8 _value) public {
+        age[_index] = _value;
+    }
+
+    //Read function, whenever we want to read/retreive a value/get info we have to use a key word "view".
+    function getAge(uint8 _index) public view returns(uint8){
+        return age[_index];
+    }
 
 }
 
